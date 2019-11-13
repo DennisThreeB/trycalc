755ec6993ea994265314c8bfeb3a7097cec54374
Dennis <dennis@threebit.io>
2019-11-13 12:00:14 +0100

    WIP PoissonBet
    Started with Poisson calculation

---
+++ trycalc/trycalc/Models/PoissonModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace trycalc.Models
+{
+    public class PoissonModel
+    {
+        public double avgHomeGoals { get; set; }
+        public double avgAwayGoals { get; set; }
+
+    }
+}
