727bd59ab38513583a3c933e00416cf215c7b9ef
Dennis <dennis@threebit.io>
2019-11-18 10:34:05 +0100

    added ProbabilityModel

---
+++ trycalc/trycalc/Models/ProbabilityBetModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace trycalc.Models
+{
+    public class ProbabilityBetModel
+    {
+        public double homeQuote { get; set; }
+        public double drawQuote { get; set; }
+        public double awayQuote { get; set; }
+        public double homeProbability { get; set; }
+        public double drawProbability { get; set; }
+        public double awayProbability { get; set; }
+    }
+}
