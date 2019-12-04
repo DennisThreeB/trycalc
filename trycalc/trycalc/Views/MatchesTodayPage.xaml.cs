0097e8ce300e3171f3c1d2c7eb8e78cdda711191
Dennis <dennis@threebit.io>
2019-12-04 13:23:34 +0100

    Added MatchesToday Page

---
+++ trycalc/trycalc/Views/MatchesTodayPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using trycalc.Models;
+using trycalc.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace trycalc.Views
+{
+    [DesignTimeVisible(false)]
+    public partial class MatchesTodayPage : ContentPage
+    {
+        public MatchesTodayPage()
+        {
+            InitializeComponent();
+        }
+
+        public async void MatchItemClicked(object sender, SelectedItemChangedEventArgs args)
+        {
+            var item = args.SelectedItem as MatchCardDataModel;
+            if (item == null)
+            {
+                return;
+            }
+           // await Navigation.PushModalAsync(new StrategieDetailPage(new StrategieDetailViewModel(item)));
+
+            //deselect item
+            TodaysMatchesListView.SelectedItem = null;
+        }
+    }
+}
